51785542fdfe72974c23914335622c1220ab2482
Elijah Gordon <braindisassemblue@gmail.com>
2023-07-17 15:46:04 +0300

    initial commit

---
+++ src/matorikkusu.py
@@ -0,0 +1,140 @@
+# Copyright 2023 Elijah Gordon (SLcK) <braindisassemblue@gmail.com>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import pygame
+import random
+
+pygame.init()
+pygame.mixer.init()
+
+characters = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'ァ', 'ア', 'ィ', 'イ', 'ゥ', 'ウ', 'ェ', 'エ', 'ォ',
+    'オ', 'カ', 'ガ', 'キ', 'ギ', 'ク', 'グ', 'ケ', 'ゲ',
+    'コ', 'ゴ', 'サ', 'ザ', 'シ', 'ジ', 'ス', 'ズ', 'セ',
+    'ゼ', 'ソ', 'ゾ', 'タ', 'ダ', 'チ', 'ヂ', 'ッ', 'ツ',
+    'ヅ', 'テ', 'デ', 'ト', 'ド', 'ナ', 'ニ', 'ヌ', 'ネ',
+    'ノ', 'ハ', 'バ', 'パ', 'ヒ', 'ビ', 'ピ', 'フ', 'ブ',
+    'プ', 'ヘ', 'ベ', 'ペ', 'ホ', 'ボ', 'ポ', 'マ', 'ミ',
+    'ム', 'メ', 'モ', 'ャ', 'ヤ', 'ュ', 'ユ', 'ョ', 'ヨ',
+    'ラ', 'リ', 'ル', 'レ', 'ロ', 'ヮ', 'ワ', 'ヰ', 'ヱ',
+    'ヲ', 'ン', 'ヴ', 'ヵ', 'ヶ', 'ヷ', 'ヸ', 'ヹ', 'ヺ',
+    '・', 'ー', 'ヽ', 'ヾ'
+]
+
+font = pygame.font.Font('../font/MS Mincho.ttf', 35)
+color = (0, 255, 0)
+
+chars = [font.render(char, True, color) for char in characters]
+
+caption = pygame.display.set_caption('(Matorikkusu)')
+
+screen = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
+display = pygame.Surface((1920, 1080))
+display.set_alpha(random.randrange(30, 40, 5))
+
+pygame.display.set_icon(pygame.image.load('../logo/logo.png'))
+
+pygame.mixer.music.load('../audio/audio.wav')
+pygame.mixer.music.play(loops=-1)
+
+
+class Matorikkusu:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        value = random.choice(chars)
+        if self.y < 1080:
+            self.y = self.y + 30
+        else:
+            self.y = -40 * random.randrange(1, 5)
+        screen.blit(value, (self.x, self.y))
+
+
+symbols = [Matorikkusu(i, random.randrange(-1020, 0)) for i in range(0, 1920, 15 * 2)]
+
+blue = (0, 0, 255)
+cyan = (0, 255, 255)
+dallas = (110, 75, 38)
+emberiea = (255, 121, 77)
+french_rose = (246, 74, 138)
+green = (0, 255, 0)
+heliotrope = (223, 115, 255)
+red = (255, 0, 0)
+white = (255, 255, 255)
+yellow = (255, 255, 0)
+magenta = (255, 0, 255)
+olive = (128, 128, 0)
+teal = (0, 128, 128)
+
+run = True
+delay = 50
+
+while run:
+    screen.blit(display, (0, 0))
+    display.fill(pygame.Color('black'))
+
+    for symbol in symbols:
+        symbol.draw()
+
+    pygame.time.delay(delay)
+    pygame.display.update()
+    pygame.time.Clock().tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                quit()
+            if event.key == pygame.K_LEFT and delay > 0:
+                delay -= 15
+            if event.key == pygame.K_RIGHT and delay < 150:
+                delay += 15
+            if event.key == pygame.K_b:
+                color = blue
+            if event.key == pygame.K_c:
+                color = cyan
+            if event.key == pygame.K_d:
+                color = dallas
+            if event.key == pygame.K_e:
+                color = emberiea
+            if event.key == pygame.K_f:
+                color = french_rose
+            if event.key == pygame.K_g:
+                color = green
+            if event.key == pygame.K_h:
+                color = heliotrope
+            if event.key == pygame.K_r:
+                color = red
+            if event.key == pygame.K_w:
+                color = white
+            if event.key == pygame.K_y:
+                color = yellow
+            if event.key == pygame.K_m:
+                color = magenta
+            if event.key == pygame.K_o:
+                color = olive
+            if event.key == pygame.K_t:
+                color = teal
+            if event.key == pygame.K_p:
+                pygame.mixer.music.pause()
+            if event.key == pygame.K_u:
+                pygame.mixer.music.unpause()
+
+            chars = [font.render(char, True, color) for char in characters]
